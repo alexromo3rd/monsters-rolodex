33d151b68c56d3b9d47f020cf233c2a4246503da
Alex Romo III <alexromo3rd@gmail.com>
2021-02-17 16:54:44 -0800

    Fetch user data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asc2'
-        },
-        {
-          name: 'Zombie',
-          id: 'asc3'
-        }
-      ]
-    };
+      monsters: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}))
   }
 
   render() {
@@ -34,4 +27,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
